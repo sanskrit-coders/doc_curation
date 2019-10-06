25a085d44dade37ab31f130bc64144abdd924dd5
vvasuki <vishvas.vasuki@gmail.com>
2019-10-06 18:22:30 +0530

    Content update

---
+++ curation_projects/raamaayana/parankusha.py
@@ -0,0 +1,40 @@
+import logging
+import os
+
+from doc_curation import parankusha
+from doc_curation.text_data import raamaayana
+
+browser = parankusha.browser
+
+def get_ramayana_text(text_id, base_dir):
+    browser.find_element_by_link_text(text_id).click()
+    # browser.implicitly_wait(2)
+    for kaanda_index in raamaayana.kaanda_indices[2:]:
+        browser.find_element_by_link_text("Kanda-%d" % kaanda_index).click()
+        if text_id == "रामायणम्-नव्यपाठः":
+            sarga_list = raamaayana.get_sarga_list_baroda(kaanda_index=kaanda_index)
+        else:
+            sarga_list = raamaayana.get_sarga_list_kumbhakonam(kaanda_index=kaanda_index)
+
+        for sarga_index in sarga_list:
+            logging.info("Kanda %d Sarga %d", kaanda_index, sarga_index)
+            browser.find_element_by_link_text("Sarga-%d" % sarga_index).click()
+            text_spans = browser.find_element_by_id("divResults").find_elements_by_tag_name("span")
+            lines = ["\n", "\n"]
+            for span in text_spans:
+                shloka = span.text
+                shloka = shloka.replace("। ", "।  \n")
+                shloka = shloka.replace("।।", " ॥ ")
+                lines.append(shloka + "  \n")
+            outfile_path = os.path.join(base_dir, str(kaanda_index), "%03d" % sarga_index + ".md")
+            os.makedirs(name=os.path.dirname(outfile_path), exist_ok=True)
+            with open(outfile_path, "w+") as outfile:
+                outfile.writelines(lines)
+
+
+if __name__ == '__main__':
+    parankusha.login()
+    get_ramayana_text(text_id="वाल्मीकिरामायणम् प्राचीनपाठः", base_dir="/home/vvasuki/vvasuki-git/kAvya/content/TIkA/padya/purANa/rAmAyaNa/kumbhakona")
+    # with open("/home/vvasuki/vvasuki-git/kAvya/content/TIkA/padya/purANa/rAmAyaNa/baroda.md", "a") as outfile:
+    #     get_ramayana_text(text_id="रामायणम्-नव्यपाठः", outfile=outfile)
+    # browser.close()
--- doc_curation/parankusha.py
+++ doc_curation/parankusha.py
@@ -7,6 +7,10 @@ from selenium.webdriver.chrome import options
 import json
 
 from doc_curation.text_data import raamaayana
+from selenium.webdriver.remote.remote_connection import LOGGER
+LOGGER.setLevel(logging.WARNING)
+from urllib3.connectionpool import log as urllibLogger
+urllibLogger.setLevel(logging.WARNING)
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -22,9 +26,9 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'local_config
     configuration = json.load(handle)
 
 configuration_parankusha = configuration['parankusha']
-browser.get("http://parankusan.cloudapp.net/Integrated/Texts.aspx")
 
 def login():
+    browser.get("http://parankusan.cloudapp.net/Integrated/Texts.aspx")
     username = browser.find_element_by_id("txtUserName")
     username.send_keys(configuration_parankusha["user"])
     browser.find_element_by_id("btnNext").click()
@@ -32,29 +36,4 @@ def login():
     browser.find_element_by_id("btnLogin").click()
     browser.get("http://parankusan.cloudapp.net/Integrated/Texts.aspx")
 
-def get_sarga_text():
-    pass
 
-def get_kumbhakona_ramayana_text(outfile):
-    browser.find_element_by_link_text("वाल्मीकिरामायणम् प्राचीनपाठः").click()
-    # browser.implicitly_wait(2)
-    for kaanda_index in raamaayana.kaanda_indices[1:]:
-        browser.find_element_by_link_text("Kanda-%d" % kaanda_index).click()
-        for sarga_index in raamaayana.get_sarga_list_kumbhakonam(kaanda_index=kaanda_index):
-            logging.info("Kanda %d Sarga %d", kaanda_index, sarga_index)
-            browser.find_element_by_link_text("Sarga-%d" % sarga_index).click()
-            text_spans = browser.find_element_by_id("divResults").find_elements_by_tag_name("span")
-            lines = ["\n", "\n"]
-            for span in text_spans:
-                shloka = span.text
-                shloka = shloka.replace("। ", "।  \n")
-                shloka = shloka.replace("।।", " ॥ ")
-                lines.append(shloka + "  \n")
-            outfile.writelines(lines)
-
-
-if __name__ == '__main__':
-    login()
-    with open("/home/vvasuki/vvasuki-git/kAvya/content/TIkA/padya/purANa/rAmAyaNa/kumbhakona.md", "a") as outfile:
-        get_kumbhakona_ramayana_text(outfile)
-    # browser.close()s
