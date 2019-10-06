0e2cf28aa1707f19e48882c3a232d709baf6e401
vvasuki <vishvas.vasuki@gmail.com>
2019-10-06 18:43:41 +0530

    Content update

--- curation_projects/raamaayana/parankusha.py
+++ curation_projects/raamaayana/parankusha.py
@@ -9,7 +9,7 @@ browser = parankusha.browser
 def get_ramayana_text(text_id, base_dir):
     browser.find_element_by_link_text(text_id).click()
     # browser.implicitly_wait(2)
-    for kaanda_index in raamaayana.kaanda_indices[2:]:
+    for kaanda_index in [2, 6, 7]:
         browser.find_element_by_link_text("Kanda-%d" % kaanda_index).click()
         if text_id == "रामायणम्-नव्यपाठः":
             sarga_list = raamaayana.get_sarga_list_baroda(kaanda_index=kaanda_index)
@@ -18,6 +18,10 @@ def get_ramayana_text(text_id, base_dir):
 
         for sarga_index in sarga_list:
             logging.info("Kanda %d Sarga %d", kaanda_index, sarga_index)
+            outfile_path = os.path.join(base_dir, str(kaanda_index), "%03d" % sarga_index + ".md")
+            if os.path.exists(outfile_path):
+                logging.info("Skipping " + outfile_path)
+                continue
             browser.find_element_by_link_text("Sarga-%d" % sarga_index).click()
             text_spans = browser.find_element_by_id("divResults").find_elements_by_tag_name("span")
             lines = ["\n", "\n"]
@@ -26,9 +30,8 @@ def get_ramayana_text(text_id, base_dir):
                 shloka = shloka.replace("। ", "।  \n")
                 shloka = shloka.replace("।।", " ॥ ")
                 lines.append(shloka + "  \n")
-            outfile_path = os.path.join(base_dir, str(kaanda_index), "%03d" % sarga_index + ".md")
             os.makedirs(name=os.path.dirname(outfile_path), exist_ok=True)
-            with open(outfile_path, "w+") as outfile:
+            with open(outfile_path, "w") as outfile:
                 outfile.writelines(lines)
 
 
@@ -37,4 +40,4 @@ if __name__ == '__main__':
     get_ramayana_text(text_id="वाल्मीकिरामायणम् प्राचीनपाठः", base_dir="/home/vvasuki/vvasuki-git/kAvya/content/TIkA/padya/purANa/rAmAyaNa/kumbhakona")
     # with open("/home/vvasuki/vvasuki-git/kAvya/content/TIkA/padya/purANa/rAmAyaNa/baroda.md", "a") as outfile:
     #     get_ramayana_text(text_id="रामायणम्-नव्यपाठः", outfile=outfile)
-    # browser.close()
+    browser.close()
