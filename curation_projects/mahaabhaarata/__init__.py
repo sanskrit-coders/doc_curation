e050c761d83f2b55e80c904289ad15b10a2b62a1
vvasuki <vishvas.vasuki@gmail.com>
2020-01-23 06:07:04 +0530

    Content update

--- curation_projects/mahaabhaarata/__init__.py
+++ curation_projects/mahaabhaarata/__init__.py
@@ -1,3 +1,30 @@
+import os
+import regex
+from doc_curation.md_helper import MdFile
+
+
+def get_adhyaaya_md_files(md_file_path):
+    md_files = MdFile.get_md_files_from_path(dir_path=md_file_path, file_pattern="**/*.md", file_name_filter=lambda x: len(regex.findall("\\d\\d\\d", os.path.basename(x))) > 0)
+    return md_files
+
+
+def get_parva_adhyaaya(md_file):
+    parva = regex.findall("/\\d\\d-", str(md_file.file_path))[0].replace("/", "").replace("-", "")
+    adhyaaya = regex.findall("\\d\\d\\d", str(md_file.file_path))[0]
+    return (parva, adhyaaya)
+
+
+def get_adhyaaya_to_source_file_map():
+    md_files = get_adhyaaya_md_files(md_file_path="/home/vvasuki/sanskrit/raw_etexts/purANa/mahAbhArata/kumbhakonam")
+    final_map = {}
+    for md_file in md_files:
+        parva = regex.findall("/\\d\\d/", str(md_file.file_path))[0].replace("/", "")
+        adhyaaya = regex.findall("\\d\\d\\d", str(md_file.file_path))[0]
+        adhyaaya_id = "%s-%s" % (parva, adhyaaya)
+        final_map[adhyaaya_id] = md_file
+    return final_map
+
+
 
 
 
---
+++ curation_projects/mahaabhaarata/update_text.py
@@ -0,0 +1,29 @@
+import regex
+
+from curation_projects import mahaabhaarata
+import logging
+
+# Remove all handlers associated with the root logger object.
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(levelname)s:%(asctime)s:%(module)s:%(lineno)d %(message)s")
+
+
+if __name__ == '__main__':
+    adhyaaya_to_source_file_map = mahaabhaarata.get_adhyaaya_to_source_file_map()
+    # logging.debug(adhyaaya_to_mp3_map)
+    dest_md_files = mahaabhaarata.get_adhyaaya_md_files(md_file_path="/home/vvasuki/vvasuki-git/kAvya/content/TIkA/padya/purANa/mahAbhArata")
+    logging.debug(dest_md_files)
+    for md_file in dest_md_files:
+        # md_file.replace_in_content("<div class=\"audioEmbed\".+?></div>\n", "")
+        logging.debug(md_file.file_path)
+        (parva, adhyaaya) = mahaabhaarata.get_parva_adhyaaya(md_file)
+        adhyaaya_id = "%s-%s" % (parva, adhyaaya)
+        logging.debug(adhyaaya_id)
+        (yml, current_content) = md_file.read_md_file()
+        audio_tag = next(iter(regex.findall("<div class.*div>", current_content.replace("\n", " "))), '')
+        (_, target_content) = adhyaaya_to_source_file_map[adhyaaya_id].read_md_file()
+        # logging.debug(adhyaaya_to_source_file_map[adhyaaya_id])
+        md_file.replace_content("%s\n\n%s" % (audio_tag, target_content), dry_run=True)
