c7df7db6dbbd91c69bda646d4101553bd1aa2f69
vvasuki <vishvas.vasuki@gmail.com>
2020-02-04 19:06:50 +0530

    Content update

--- curation_projects/taittiriiya/dump.py
+++ curation_projects/taittiriiya/dump.py
@@ -8,19 +8,33 @@ from selenium.common.exceptions import NoSuchElementException
 from doc_curation import parankusha, text_data
 
 
+def open_path(subunit_path, unit_data):
+    logging.debug(list(zip(subunit_path, unit_data["unitNameListInSite"])))
+    for (subunit, unitNameInSite) in zip(subunit_path, unit_data["unitNameListInSite"]):
+        element_text = "%s%d" % (unitNameInSite, subunit)
+        parankusha.click_link_by_text(browser=browser, element_text=element_text)
+
+
+def close_path(subunit_path, unit_data):
+    logging.info(list(zip(reversed(subunit_path), reversed(unit_data["unitNameListInSite"]))))
+    for (subunit, unitNameInSite) in list(zip(reversed(subunit_path), reversed(unit_data["unitNameListInSite"]))):
+        element_text = "%s%d" % (unitNameInSite, subunit)
+        logging.info(element_text)
+        parankusha.click_link_by_text(browser=browser, element_text=element_text)
+
+
 def get_text(browser, text_id, base_dir, unit_info_file):
     parankusha.click_link_by_text(browser=browser, element_text=text_id)
     unit_data = text_data.get_subunit_data(unit_info_file, [])
     
     for subunit_path in text_data.get_subunit_path_list(json_file=unit_info_file, unit_path_list=[]):
         try:
-            for (subunit, unitNameInSite) in zip(subunit_path, unit_data["unitNameListInSite"]):
-                element_text = "%s%d" % (unitNameInSite, subunit)
-                parankusha.click_link_by_text(browser=browser, element_text=element_text)
+            open_path(subunit_path=subunit_path, unit_data=unit_data)
         except NoSuchElementException as e:
+            close_path(subunit_path=subunit_path, unit_data=unit_data)
+            exit()
             logging.warning("Skipping as Could not find element " + str(traceback.format_exc()))
             continue
-        logging.info(list(zip(unit_data["unitNameListInSite"], subunit_path)))
         outfile_path = os.path.join(base_dir, "/".join(map(str, subunit_path)) + ".md")
         if os.path.exists(outfile_path):
             logging.info("Skipping " + outfile_path)
@@ -33,8 +47,7 @@ def get_text(browser, text_id, base_dir, unit_info_file):
             with open(outfile_path, "w") as outfile:
                 outfile.writelines(lines)
         # Close the kANDa - else the driver may pick sarga from this kANDa when it is to pick the sarga from the next kANDa?!
-        element_text = "%s%d" % (unit_data["unitNameListInSite"][0], subunit_path[0])
-        parankusha.click_link_by_text(browser=browser, element_text=element_text)
+        close_path(subunit_path=subunit_path, unit_data=unit_data)
 
 
 def get_taittiriiya():
@@ -43,9 +56,15 @@ def get_taittiriiya():
     # get_text(browser=browser, text_id="संहितायाः भट्टभास्करभाष्यम्", base_dir="/home/vvasuki/sanskrit/raw_etexts/veda/taittirIya/bhaTTa-bhAskara/saMhitA", unit_info_file=os.path.join(os.path.dirname(text_data.__file__), "veda/taittirIya/bhAShya/bhaTTa-bhAskara/saMhitA.json"))
     # get_text(browser=browser, text_id="संहितायाः सायणभाष्यम्", base_dir="/home/vvasuki/sanskrit/raw_etexts/veda/taittirIya/sAyaNa/saMhitA", unit_info_file=os.path.join(os.path.dirname(text_data.__file__), "veda/taittirIya/bhAShya/bhaTTa-bhAskara/saMhitA.json"))
     # get_text(browser=browser, text_id="संहिता", base_dir="/home/vvasuki/sanskrit/raw_etexts/veda/taittirIya/mUlam/saMhitA", unit_info_file=os.path.join(os.path.dirname(text_data.__file__), "veda/taittirIya/mUlam/saMhitA.json"))
+
     # get_text(browser=browser, text_id="ब्राह्मणम्", base_dir="/home/vvasuki/sanskrit/raw_etexts/veda/taittirIya/mUlam/brAhmaNam", unit_info_file=os.path.join(os.path.dirname(text_data.__file__), "veda/taittirIya/mUlam/brAhmaNa.json"))
+    # get_text(browser=browser, text_id="ब्राह्मणस्य भट्टभास्करभाष्यम्", base_dir="/home/vvasuki/sanskrit/raw_etexts/veda/taittirIya/bhaTTa-bhAskara/brAhmaNam/", unit_info_file=os.path.join(os.path.dirname(text_data.__file__), "veda/taittirIya/bhAShya/bhaTTa-bhAskara/brAhmaNa.json"))
+
     # get_text(browser=browser, text_id="आरण्यकम्", base_dir="/home/vvasuki/sanskrit/raw_etexts/veda/taittirIya/mUlam/AraNyakam", unit_info_file=os.path.join(os.path.dirname(text_data.__file__), "veda/taittirIya/mUlam/AraNyaka.json"))
+    # get_text(browser=browser, text_id="आरण्यकस्य भट्टभास्करभाष्यम्", base_dir="/home/vvasuki/sanskrit/raw_etexts/veda/taittirIya/bhaTTa-bhAskara/AraNyakam/", unit_info_file=os.path.join(os.path.dirname(text_data.__file__), "veda/taittirIya/bhAShya/bhaTTa-bhAskara/AraNyaka.json"))
+
     # get_text(browser=browser, text_id="काठकम्", base_dir="/home/vvasuki/sanskrit/raw_etexts/veda/taittirIya/mUlam/kAThakam", unit_info_file=os.path.join(os.path.dirname(text_data.__file__), "veda/taittirIya/mUlam/kAThaka.json"))
+    get_text(browser=browser, text_id="काठकस्य भट्टभास्करभाष्यम्", base_dir="/home/vvasuki/sanskrit/raw_etexts/veda/taittirIya/bhaTTa-bhAskara/kAThakam/", unit_info_file=os.path.join(os.path.dirname(text_data.__file__), "veda/taittirIya/bhAShya/bhaTTa-bhAskara/kAThaka.json"))
     
 
 if __name__ == '__main__':
