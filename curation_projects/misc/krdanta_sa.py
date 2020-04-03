8f8fcb1741a339c7b663c75efaa8418f7fc92c13
vvasuki <vishvas.vasuki@gmail.com>
2020-04-03 14:58:23 +0530

    CU

--- curation_projects/misc/krdanta_sa.py
+++ curation_projects/misc/krdanta_sa.py
@@ -2,7 +2,6 @@ import logging
 import os
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome import options
 from selenium.webdriver.remote.remote_connection import LOGGER
 
@@ -15,18 +14,18 @@ logging.basicConfig(
     format="%(levelname)s:%(asctime)s:%(module)s:%(lineno)d %(message)s")
 
 opts = options.Options()
-opts.headless = False
+opts.headless = True
 browser = webdriver.Chrome(options=opts)
 browser.implicitly_wait(6)
 
 
 def get_item(item_url):
     logging.info(item_url)
-    item_browser = webdriver.Chrome()
+    item_browser = webdriver.Chrome(options=opts)
     item_browser.implicitly_wait(6)
     item_browser.get(item_url)
-    root = browser.find_element_by_tag_name("h2").text.replace("कृदन्त - ", "").strip()
-    data_rows = browser.find_element_by_css_selector(".declensionContainer").find_elements_by_css_selector(".row")
+    root = item_browser.find_element_by_tag_name("h2").text.replace("कृदन्त - ", "").strip()
+    data_rows = item_browser.find_element_by_css_selector(".declension").find_elements_by_css_selector(".row")
     body_data = [root]
     headwords = [root]
     for row in data_rows:
@@ -48,13 +47,19 @@ def get_entries_from_list(list_id):
         item_url = item_element.get_attribute("href")
         item = get_item(item_url)
         items.append(item)
-        browser.get(list_url)
-    logging.debug(items)
+    # logging.debug(items)
     return items
+
+
+def dump_dict(outfile_path):
+    os.makedirs(name=os.path.dirname(outfile_path), exist_ok=True)
+    with open(outfile_path, "w") as outfile:
+        for list_id in range(1, 13):
+            items = get_entries_from_list(list_id=list_id)
+            for item in items:
+                outfile.writelines(["|".join(item[0]), item[1]], "")
 
 
 if __name__ == '__main__':
     # logging.debug(get_item("http://sanskritabhyas.in/hi/Kridanta/View/%E0%A4%AD%E0%A5%82"))
-    for list_id in range(1, 3):
-        items = get_entries_from_list(list_id=list_id)
-        
+    dump_dict("/home/vvasuki/indic-dict/stardict-sanskrit-vyAkaraNa/kRdanta-sa/kRdanta-sa.babylon")
