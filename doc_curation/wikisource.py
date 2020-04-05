4cdc1c2aeae6e7cd45b8605a2588436e86b2d7a5
vvasuki <vishvas.vasuki@gmail.com>
2020-04-05 20:23:23 +0530

    CU

--- doc_curation/wikisource.py
+++ doc_curation/wikisource.py
@@ -54,7 +54,7 @@ def dump_text(url_base, num_parts, dir_path, url_id_padding="%d"):
     for id in range(1, num_parts+1):
         outfile_path = os.path.join(dir_path, "%03d.md" % id)
         title = sanscript.transliterate("%03d" % id, sanscript.SLP1, sanscript.DEVANAGARI)
-        item_url = "%s/%s" % (url_base, get_item_url_suffix(id=id, url_id_padding=url_id_padding, id_base=url_base))
+        item_url = "https://sa.wikisource.org/wiki/%s" % (get_item_url_suffix(id=id, url_id_padding=url_id_padding, id_base=url_base))
         dump_item(title=title, outfile_path=outfile_path, item_url=item_url)
 
 
@@ -63,13 +63,20 @@ def get_wiki_path(subunit_path, unit_data, url_id_padding="%2d"):
     logging.debug(list(zip(subunit_path, unit_data["unitNameListInSite"])))
     path_elements = []
     for (subunit, unitNameInSite) in zip(subunit_path, unit_data["unitNameListInSite"]):
-        element_text = get_item_url_suffix(id=id, id_base=unitNameInSite, url_id_padding=url_id_padding)
+        element_text = get_item_url_suffix(id=subunit, id_base=unitNameInSite, url_id_padding=url_id_padding)
         path_elements.append(element_text)
     return "/".join(path_elements)
 
 
-def dump_text(url_base, dir_path, unit_info_file):
+def dump_deep_text(url_text_id, url_leaf_id_padding, dir_path, unit_info_file, dry_run=False):
     unit_data = text_data.get_subunit_data(unit_info_file, [])
     for subunit_path in text_data.get_subunit_path_list(json_file=unit_info_file, unit_path_list=[]):
         relative_dir_path = "/".join(["%02d" % x for x in subunit_path[:-1]])
         outfile_path = os.path.join(dir_path, relative_dir_path, "%03d.md" % subunit_path[-1])
+        import urllib
+        item_url = "https://sa.wikisource.org/wiki/%s/%s" % (urllib.parse.quote(url_text_id), get_wiki_path(subunit_path=subunit_path, unit_data=unit_data, url_id_padding=url_leaf_id_padding))
+        title = sanscript.transliterate("%03d" % subunit_path[-1], sanscript.SLP1, sanscript.DEVANAGARI)
+        logging.info("Getting %s to %s with title %s", item_url, outfile_path, title)
+        if not dry_run:
+            dump_item(title=title, outfile_path=outfile_path, item_url=item_url)
+
