5a2ba2ff32e8308f115403bfc2f034c8a085ba89
vvasuki <vishvas.vasuki@gmail.com>
2020-04-20 12:38:11 +0530

    CU

--- curation_projects/__main__.py
+++ curation_projects/__main__.py
@@ -2,4 +2,4 @@ from doc_curation.md_helper import MdFile
 
 # MdFile.fix_index_files(dir_path="/home/vvasuki/vvasuki-git/kAvya/content/TIkA/padyam/kathAsaritsAgaraH/", dry_run=False)
 
-MdFile.set_titles_from_filenames(dir_path="/home/vvasuki/vvasuki-git/kAvya/content/TIkA/padyam/kathAsaritsAgaraH/12/vetAla-panchavimshatikA", dry_run=False)
+MdFile.set_titles_from_filenames(dir_path="/home/vvasuki/vvasuki-git/kAvya/content/TIkA/padyam/kathAsaritsAgaraH/12/", dry_run=False)
