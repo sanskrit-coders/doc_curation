6529349ffe102fd05fb8ffee908313ba998f2f65
vvasuki <vishvas.vasuki@gmail.com>
2020-04-25 14:29:31 +0530

    CU

--- curation_projects/dicts/dump_jnu_tinanta.py
+++ curation_projects/dicts/dump_jnu_tinanta.py
@@ -3,5 +3,5 @@ import subprocess
 for i in range(1,451):
   url = "http://sanskrit.jnu.ac.in/tinanta/tinanta.jsp?t=%d" % i
   cmd = "lynx -dump " + url + "> ~/tinanta/%d" % i
-  print cmd
+  # print cmd
   subprocess.call(cmd, shell=True)
