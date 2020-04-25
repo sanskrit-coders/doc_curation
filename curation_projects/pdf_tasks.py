d0f4197082d9952240fab139049ce883afc9d738
vvasuki <vishvas.vasuki@gmail.com>
2020-04-25 09:52:50 +0530

    CU

--- curation_projects/pdf_tasks.py
+++ curation_projects/pdf_tasks.py
@@ -1,4 +1,4 @@
 from doc_curation import pdf
 
 if __name__ == '__main__':
-    pdf.split_and_ocr_on_drive(pdf_path="/home/vvasuki/Documents/books/granthasangrahaH/mImAmsA/Mimansa Paribhasha - Shri Krishna Yajva.pdf")
+    pdf.split_and_ocr_on_drive(pdf_path="/home/vvasuki/Documents/books/granthasangrahaH/mImAmsA/jaiminI-nyAya-mAlA.pdf")
