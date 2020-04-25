967d8af48c3f0de5beac161a3cc6c66a91185904
vvasuki <vishvas.vasuki@gmail.com>
2020-04-25 13:41:23 +0530

    CU

--- curation_projects/pdf_tasks.py
+++ curation_projects/pdf_tasks.py
@@ -1,4 +1,4 @@
 from doc_curation import pdf
 
 if __name__ == '__main__':
-    pdf.split_and_ocr_on_drive(pdf_path="/home/vvasuki/Documents/books/granthasangrahaH/mImAmsA/jaiminI-nyAya-mAlA.pdf")
+    pdf.split_and_ocr_on_drive(pdf_path="/home/vvasuki/Documents/books/granthasangrahaH/koshaH/tulsi_shabda_kosh_part_02.pdf")
