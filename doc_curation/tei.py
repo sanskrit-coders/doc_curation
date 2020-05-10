17acfaae9119c6a3f7492a84afe8b14208d99845
vvasuki <vishvas.vasuki@gmail.com>
2020-05-10 22:11:48 +0530

    CU

--- doc_curation/tei.py
+++ doc_curation/tei.py
@@ -10,12 +10,3 @@ def dump_md(tei_path, md_path):
     # subprocess.call([os.path.join(os.path.dirname(__file__), "tei_xsl/bin/transformtei"),"--odd", tei_path, md_path])
     subprocess.call(["saxon", "-o:"+md_path, "-s:"+tei_path, os.path.join(os.path.dirname(__file__), "tei_xsl/markdown/tei-to-markdown-sarit.xsl")])
 
-
-def dump_md_with_lxml(tei_path, md_path):
-    dom = ET.parse(tei_path)
-    xslt = ET.parse( os.path.join(os.path.dirname(__file__), "tei_xsl/markdown/tei-to-markdown.xsl"))
-    transformer = ET.XSLT(xslt)
-    newdom = transformer(dom)
-    content = ET.tostring(newdom, pretty_print=True)
-    md_file = md_helper.MdFile(file_path=md_path)
-    md_file.dump_to_file(yml={}, md=content)
