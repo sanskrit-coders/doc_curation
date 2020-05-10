a71f1d5ef8a2bd3db3bc1f13578a3854f5bf86a7
vvasuki <vishvas.vasuki@gmail.com>
2020-05-10 21:50:03 +0530

    CU

--- doc_curation/tei.py
+++ doc_curation/tei.py
@@ -8,7 +8,7 @@ from doc_curation import md_helper
 def dump_md(tei_path, md_path):
     import subprocess
     # subprocess.call([os.path.join(os.path.dirname(__file__), "tei_xsl/bin/transformtei"),"--odd", tei_path, md_path])
-    subprocess.call(["saxon", "-o:"+md_path, "-s:"+tei_path, os.path.join(os.path.dirname(__file__), "tei_xsl/markdown/tei-to-markdown.xsl")])
+    subprocess.call(["saxon", "-o:"+md_path, "-s:"+tei_path, os.path.join(os.path.dirname(__file__), "tei_xsl/markdown/tei-to-markdown-sarit.xsl")])
 
 
 def dump_md_with_lxml(tei_path, md_path):
