c8774389566b34907b17e296273762c8edc56f47
vvasuki <vishvas.vasuki@gmail.com>
2020-05-18 21:40:57 +0530

    CU

--- doc_curation/scraping/pandanus.py
+++ doc_curation/scraping/pandanus.py
@@ -27,28 +27,34 @@ def rewind(init_url):
     first_line_anchor = browser.find_element_by_css_selector(css_selector="td.utf>a")
     previous_page_url = first_line_anchor.get_attribute("href")
     if previous_page_url != init_url:
-        rewind(init_url=previous_page_url)
+        return rewind(init_url=previous_page_url)
     else:
+        logging.info("Done rewinding to %s", init_url)
         return init_url
 
 
-def dump_text(init_url, out_path):
-    def get_text_and_url(init_url):
-        browser.get(url=init_url)
+def dump_text(init_url, out_path, overwrite=True):
+    if overwrite == False and os.path.exists(out_path):
+        logging.info("Not overwriting %s to %s", init_url, out_path)
+        return
+    logging.info("Dumping %s to %s", init_url, out_path)
+    init_url = rewind(init_url)
+    def get_text_and_url(url):
+        browser.get(url=url)
         next_url = browser.find_elements_by_css_selector(css_selector="td.utf>a")[-1].get_attribute("href")
-        if next_url == init_url:
+        if next_url == url:
             logging.info("Finished.")
             return ("", None)
         text = browser.find_elements_by_css_selector(css_selector="td.utf+td.utf")[-2].text
         text = sanscript.transliterate(data=text, _from=sanscript.IAST, _to=sanscript.DEVANAGARI)
-        logging.debug(text)
+        # logging.debug(text)
         return (text, next_url)
     os.makedirs(os.path.dirname(out_path), exist_ok=True)
     
     next_url = init_url
     with codecs.open(out_path, "w", 'utf-8') as file_out:
         while next_url is not None:
-            (text, next_url) = get_text_and_url(init_url=next_url)
+            (text, next_url) = get_text_and_url(url=next_url)
             logging.debug(text)
             file_out.write(text + "  \n")
 
