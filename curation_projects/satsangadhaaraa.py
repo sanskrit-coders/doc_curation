df68b0a2607bd5fe0be70c1603d1efe84c8171ae
vvasuki <vishvas.vasuki@gmail.com>
2020-05-19 16:22:52 +0530

    CU

--- curation_projects/satsangadhaaraa.py
+++ curation_projects/satsangadhaaraa.py
@@ -1,5 +1,14 @@
 from doc_curation.scraping import satsangadhaaraa
 
 if __name__ == '__main__':
-    satsangadhaaraa.dump_book(init_url="http://satsangdhara.net/devi/intro-devi.htm", out_path="/home/vvasuki/sanskrit/raw_etexts/purANam/devI-mahApurANam/", overwrite=False)
-    
+    # satsangadhaaraa.dump_book(init_url="http://satsangdhara.net/devi/intro-devi.htm", out_path="/home/vvasuki/sanskrit/raw_etexts/purANam/devI-mahApurANam/", overwrite=False)
+    # satsangadhaaraa.dump_book(init_url="http://satsangdhara.net/garg/intro-garg.htm", out_path="/home/vvasuki/sanskrit/raw_etexts/purANam/gArga-saMhitA/", overwrite=False)
+    # satsangadhaaraa.dump_book(init_url="http://satsangdhara.net/vp/intro-vishnu.htm", out_path="/home/vvasuki/sanskrit/raw_etexts/purANam/viShNu-purANam/", overwrite=False)
+    # satsangadhaaraa.dump_book(init_url="http://satsangdhara.net/shiva/intro-shiva.htm", out_path="/home/vvasuki/sanskrit/raw_etexts/purANam/shiva-mahApurANam/", overwrite=False)
+    # satsangadhaaraa.dump_book(init_url="http://satsangdhara.net/harivamsh/intro-harivamsh.htm", out_path="/home/vvasuki/sanskrit/raw_etexts/purANam/harivaMsha-purANam/", overwrite=False)
+    # satsangadhaaraa.dump_book(init_url="http://satsangdhara.net/upa/intro-vaishnavo.htm", out_path="/home/vvasuki/sanskrit/raw_etexts/veda/misc/upaniShat/vaiShNavI", overwrite=False)
+    # satsangadhaaraa.dump_book(init_url="http://satsangdhara.net/upa/intro-shaiva.htm", out_path="/home/vvasuki/sanskrit/raw_etexts/veda/misc/upaniShat/shaivI", overwrite=False)
+    # satsangadhaaraa.dump_book(init_url="http://satsangdhara.net/upa/intro-shakta.htm", out_path="/home/vvasuki/sanskrit/raw_etexts/veda/misc/upaniShat/shAktA", overwrite=False)
+    # satsangadhaaraa.dump_book(init_url="http://satsangdhara.net/upa/intro-samanya.htm", out_path="/home/vvasuki/sanskrit/raw_etexts/veda/misc/upaniShat/sAmAnyA", overwrite=False)
+    # satsangadhaaraa.dump_book(init_url="http://satsangdhara.net/upa/intro-sanyasa.htm", out_path="/home/vvasuki/sanskrit/raw_etexts/veda/misc/upaniShat/saMnyAsI", overwrite=False)
+    satsangadhaaraa.dump_book(init_url="http://satsangdhara.net/upa/intro-yogo.htm", out_path="/home/vvasuki/sanskrit/raw_etexts/veda/misc/upaniShat/yaugikI", overwrite=False)
