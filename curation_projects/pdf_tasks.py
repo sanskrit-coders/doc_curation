94ee8ee5b9c163f11a0c831c55faccf1b6e38127
vvasuki <vishvas.vasuki@gmail.com>
2020-06-02 08:32:11 +0530

    CU

--- curation_projects/pdf_tasks.py
+++ curation_projects/pdf_tasks.py
@@ -10,6 +10,10 @@ logging.basicConfig(
     format="%(levelname)s:%(asctime)s:%(module)s:%(lineno)d %(message)s")
 
 
+def get_from_archive(item_id):
+    pass
+
+
 if __name__ == '__main__':
-    # pdf.split_and_ocr_on_drive(pdf_path="/home/vvasuki/Documents/books/granthasangrahaH/mImAmsA/laukika-nyAyAH/Laukika Nyaya Kosa Pramod Ganesh Lele Madhusudan Penna Samskrita Bharati.pdf", small_pdf_pages=10, start_page=86)
-    pdf.split_and_ocr_all(dir_path="/home/vvasuki/Documents/books/granthasangrahaH/mImAmsA/laukika-nyAyAH", small_pdf_pages=10)
+    pdf.split_and_ocr_on_drive(pdf_path="/home/vvasuki/Documents/books/granthasangrahaH/history/kShatriyas_in_kali_gAgAbhaTTa_opponents_mAdhav_deshpanDe.pdf", small_pdf_pages=10, start_page=1)
+    # pdf.split_and_ocr_all(dir_path="/home/vvasuki/Documents/books/granthasangrahaH/mImAmsA/laukika-nyAyAH", small_pdf_pages=10)
