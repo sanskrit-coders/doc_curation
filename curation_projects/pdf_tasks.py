8e804a281fc5031e4acd8dc98854bad557adfae5
vvasuki <vishvas.vasuki@gmail.com>
2020-09-02 16:12:28 +0530

    CU

--- curation_projects/pdf_tasks.py
+++ curation_projects/pdf_tasks.py
@@ -15,7 +15,7 @@ def get_from_archive(item_id):
 
 
 if __name__ == '__main__':
-    pdf.split_and_ocr_on_drive(pdf_path="/home/vvasuki/Documents/books/granthasangrahaH/jyotiSham/tilak-orion.pdf", small_pdf_pages=10, start_page=1, pdf_compression_power=0)
+    pdf.split_and_ocr_on_drive(pdf_path="/home/vvasuki/Documents/books/granthasangrahaH/kAvyam/gadyam/rni_in_armenia.pdf", small_pdf_pages=10, start_page=1, pdf_compression_power=0)
     # pdf.split_and_ocr_all(dir_path="/home/vvasuki/Documents/books/granthasangrahaH/vyAkaraNam/prathamAvRtti/", small_pdf_pages=10)
     # pdf.split_and_ocr_all(dir_path="/home/vvasuki/Documents/books/granthasangrahaH/vyAkaraNam/vAsu/", small_pdf_pages=10)
 
