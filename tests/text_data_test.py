0c28adbf4d58960227a1a0cb4a3b914d52910103
vvasuki <vishvas.vasuki@gmail.com>
2020-09-21 16:54:12 +0530

    CU

--- tests/text_data_test.py
+++ tests/text_data_test.py
@@ -1,8 +1,4 @@
-import json
-import logging
 import os
-
-import pytest
 
 from doc_curation import text_data
 
@@ -11,6 +7,7 @@ def test_get_subunit_list():
     unit_info_file = os.path.join(os.path.dirname(text_data.__file__), "vedaH/vAjasaneyi/shatapatha.json")
     assert text_data.get_subunit_list(json_file=unit_info_file, unit_path_list=[]) == range(1, 15)
     assert text_data.get_subunit_list(json_file=unit_info_file, unit_path_list=[2]) == range(1, 7)
+
 
 def test_get_subunit_path_list():
     unit_info_file = os.path.join(os.path.dirname(text_data.__file__), "vedaH/taittirIya/bhAShya/bhaTTa-bhAskara/saMhitA.json")
