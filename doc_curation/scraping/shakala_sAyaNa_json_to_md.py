ca3c0590e77ad5d7b1a3d89a7c86653528fba3e4
vvasuki <vishvas.vasuki@gmail.com>
2020-12-10 19:45:32 +0530

    CU

---
+++ doc_curation/scraping/shakala_sAyaNa_json_to_md.py
@@ -0,0 +1,43 @@
+import codecs
+import glob
+import json
+import logging
+import os
+
+import regex
+from indic_transliteration import sanscript
+
+from doc_curation.md_helper import MdFile
+
+
+dest_dir = "/home/vvasuki/vvasuki-git/saMskAra/content/sangrahaH/shAkalA/saMhitA/sAyaNa-bhAShyam/"
+
+
+def transform():
+  json_paths = glob.glob("/home/vvasuki/sanskrit/raw_etexts/veda/Rg/shakala/saMhitA/sAyaNabhAShyam/*/*/*.json", recursive=True)
+  suukta_id_to_md = {}
+  for json_path in sorted(json_paths):
+    with codecs.open(json_path, "r") as fp:
+      rk = json.load(fp)
+      suukta_id = "%02d/%03d" % (int(rk["classification"]["mandala"]), int(rk["classification"]["sukta"]))
+      suukta_md = suukta_id_to_md.get(suukta_id, "")
+      bhaashya = regex.sub("<.+?>", "", rk["sayanaBhashya"])
+      heading_str = sanscript.transliterate("%02d" % int(rk["classification"]["rik"]), sanscript.IAST, sanscript.DEVANAGARI)
+      attribute_str = "## %s\n%s। %s। %s।" % (heading_str, rk["attribute"]["devata"], rk["attribute"]["rishi"], rk["attribute"]["chandas"])
+      rk_md = "%s\n\n%s\n\n%s\n\n%s" % (attribute_str, "  \n".join(rk["samhitaAux"]["lines"]), "  \n".join(rk["padapaatha"]["lines"]), bhaashya)
+      suukta_md += "\n\n%s" % rk_md
+      if bhaashya == "":
+        logging.warning("No bhAShya for %s", rk["id"])
+      suukta_id_to_md[suukta_id] = suukta_md
+
+  for suukta_id in suukta_id_to_md.keys():
+    dest_path = os.path.join(dest_dir, suukta_id + ".md")
+    md_file = MdFile(file_path=dest_path)
+    title = sanscript.transliterate(suukta_id.split("/")[-1], sanscript.IAST, sanscript.DEVANAGARI)
+    md_file.dump_to_file(metadata={"title": title}, md=suukta_id_to_md[suukta_id], dry_run=False)
+
+
+
+if __name__ == '__main__':
+    transform()
+    # MdFile.fix_index_files(dest_dir)
