e3e2082e65e16d6de0547061ed59d1f258c19627
vvasuki <vishvas.vasuki@gmail.com>
2021-01-08 22:04:38 +0530

    CU

--- curation_projects/veda/vedanidhi.py
+++ curation_projects/veda/vedanidhi.py
@@ -1,10 +1,13 @@
+import logging
 import time
 
+import regex
 from selenium.webdriver.support.select import Select
 
 from doc_curation import md_helper
 from doc_curation.scraping import vedanidhi, html
 from doc_curation.scraping.html import selenium
+from indic_transliteration import sanscript
 
 
 def select_part(browser, veda, shaakhaa, text, division=None, chapter=None, para=None, cluster=None):
@@ -38,25 +41,32 @@ def get_page_text(browser):
 
 
 def replace_private_space_characters(text):
-  text = text.replace("", "꣡").replace("", "꣡꣯").replace("", "꣢").replace("", "꣢꣯").replace("", "꣣").replace("", "꣤").replace("", "꣤꣯").replace("", "꣥").replace("", "꣥꣯").replace("", "꣯")
-  text = text.replace("ख़", "ऽ᳒२᳒").replace("ग़", "᳐").replace("ढ़", "꣭")
+  text = text.replace("", "꣡").replace("", "꣡꣯").replace("", "꣢").replace("", "꣢꣯").replace("", "꣣").replace("", "꣣꣯").replace("", "꣤").replace("", "꣤꣯").replace("", "꣥").replace("", "꣥꣯").replace("", "꣯")
+  # क़ 	ख़ 	ग़ 	ज़ 	ड़ 	ढ़ 	फ़ 	य़
+  text = text.replace("ख़", "ऽ᳒२᳒").replace("ग़", "᳐").replace("ढ़", "꣭").replace("ड़", "꣰").replace("ज़", "꣰꣯").replace("य़", "꣪")
   text = text.replace("", "ः").replace("", "ᳲ").replace("", "ऽ").replace("", "ऽऽ").replace("", "ꣳ")
   # अग्नि.मीळे पुरोहितँ- यज्ञस्य देव.मृत्विजम्। होतारं रत्नधातमम्।।  
   # वाय उक्थेभि र्जरन्ते- त्वा.मच्छा जरितारः। सुतसोमाˆ अहर्विदः।
   # वृषा यूथेव वंसग- कृष्टी.रिय.र्त्योजसा। ईशानो अप्रति¤ष्कुतः।।   तुञ्जेतुञ्जे य उत्तरे- स्तोमाˆ इन्द्रस्य हो
   text = text.replace("", "॒").replace("", "᳚").replace("", "॑").replace("", "").replace("", "").replace("", "").replace("", "").replace("¤", "")
+  if "ऽ" in text:
+    # Convert digits
+    text = sanscript.transliterate(text, _from=sanscript.HK, _to=sanscript.DEVANAGARI)
+    text = regex.sub("([॒᳚॑꣡꣯꣢꣣꣤꣥᳒᳐])([ःᳲꣳ])", r"\2\1", text)
   return text
 
 def dump_text(browser, title, out_file_path):
+  previous_page_text = None
   text = ""
-  previous_text = None
   while True:
-    text = text + get_page_text(browser=browser)
-    if text == previous_text:
+    page_text = get_page_text(browser=browser)
+    if previous_page_text == page_text:
       break
+    logging.debug(page_text)
+    text = text + page_text
     time.sleep(2)
     selenium.click_link_by_text(browser=browser, element_text="Next")
-    previous_text = text
+    previous_page_text = page_text
 
   md_file = md_helper.MdFile(file_path=out_file_path)
   md_file.dump_to_file(metadata={"title": title}, md=text, dry_run=False)
