8c2d700fb0ad0e1d2852b483b71ef4f5304f6b21
Vishvas Vasuki विश्वासः <vishvas.vasuki@gmail.com>
2022-10-16 21:29:41 +0530

    CU

--- doc_curation_projects/general_tasks/pdf_tasks.py
+++ doc_curation_projects/general_tasks/pdf_tasks.py
@@ -24,5 +24,5 @@ if __name__ == '__main__':
     #     doc_curation.pdf.drive_ocr.split_and_ocr_on_drive(pdf_path="/home/vvasuki/Documents/books/granthasangrahaH/history/New-History-Of-The-Marathas-Vol%d.pdf" % i,  small_pdf_pages=10, start_page=1, detext=False)
     # doc_curation.pdf.drive_ocr.split_and_ocr_on_drive(pdf_path="", small_pdf_pages=10, detext=False)
     # doc_curation.pdf.drive_ocr.split_and_ocr_all(dir_path="/home/vvasuki/Documents/books/granthasangrahaH/kAvyam/stotram/shrIvaiShNavAH/4kDivyaPrabandham", small_pdf_pages=10, detext=True)
-    doc_curation.pdf.drive_ocr.split_and_ocr_all( dir_path="/home/vvasuki/Documents/books/granthasangrahaH/kalpaH/Baudhayana-Prayoga-Ratna-Mala.pdf", small_pdf_pages=5, detext=False)
+    doc_curation.pdf.drive_ocr.split_and_ocr_all( dir_path="/home/vvasuki/Documents/books/granthasangrahaH/bhAShA/sanskrit", small_pdf_pages=5, detext=False)
     # doc_curation.pdf.drive_ocr.split_and_ocr_all(dir_path="/home/vvasuki/Documents/books/granthasangrahaH/vyAkaraNam/deshapANDeya-mAdhavaH/", small_pdf_pages=10, detext=False)
---
+++ doc_curation_projects/veda/yajuH/vaajasaneyi/shatapatha/vamshiidhara.py
@@ -0,0 +1,26 @@
+
+import logging
+import os, regex
+
+from doc_curation_projects.veda.yajuH.vaajasaneyi import shatapatha
+from indic_transliteration import sanscript
+from indic_transliteration.sanscript.schemes import brahmic
+from indic_transliteration.sanscript.schemes.brahmic import accent
+
+from doc_curation.scraping.misc_sites import titus
+from doc_curation.md import library, content_processor
+from doc_curation.md.library import metadata_helper
+from doc_curation import book_data
+from doc_curation.md.file import MdFile
+
+base_dir = os.path.join(shatapatha.CONTENT_BASE, "vaMshIdhara-pAThaH/sasvaram")
+devanagari = sanscript.SCHEMES[sanscript.DEVANAGARI]
+
+
+
+
+
+
+if __name__ == '__main__':
+  library.apply_function(fn=content_processor.replace_texts, dir_path=base_dir, patterns=["ॗ"], replacement="᳘")
+  pass
