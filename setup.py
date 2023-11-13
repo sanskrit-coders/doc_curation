408ee0e744bf861ee5b03c5b56ac9dd706e6ac34
vvasuki <vishvas.vasuki@gmail.com>
2023-11-13 15:12:27 +0530

    CU

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.18',
+    version='0.1.19',
 
     description='A package for curating doc file collections, with ability to sync with youtube and archive.org doc items.',
     long_description=long_description,
