e7649158b2f0e42071aed1a3dc3897b4db3ff8d6
vvasuki <vishvas.vasuki@gmail.com>
2024-02-11 10:13:06 +0530

    CU

--- doc_curation_projects/general_tasks/pdf_tasks.py
+++ doc_curation_projects/general_tasks/pdf_tasks.py
@@ -17,15 +17,15 @@ def get_from_archive(item_id):
 if __name__ == '__main__':
     pass
     # drive_ocr.ocr_all(dir_path="")
-    # drive_ocr.split_and_ocr_on_drive(pdf_path="/run/media/vvasuki/vData/text/granthasangrahaH/kAvyam/bannanje/tarka-kesarI_paDamunnUru-nArAyaNAchAryaH.pdf", small_pdf_pages=10, start_page=1, detext=False)
+    # drive_ocr.split_and_ocr_on_drive(pdf_path="/media/vvasuki/vData/text/granthasangrahaH/kAvyam/bannanje/tarka-kesarI_paDamunnUru-nArAyaNAchAryaH.pdf", small_pdf_pages=10, start_page=1, detext=False)
 
-    # drive_ocr.split_and_ocr_on_drive(pdf_path="/run/media/vvasuki/vData/text/granthasangrahaH/kAvyam/bannaje/tarka-kesarI_paDamunnUru-nArAyaNAchAryaH.pdf", small_pdf_pages=10, start_page=264, end_page=None, detext=True)
+    # drive_ocr.split_and_ocr_on_drive(pdf_path="/media/vvasuki/vData/text/granthasangrahaH/kAvyam/bannaje/tarka-kesarI_paDamunnUru-nArAyaNAchAryaH.pdf", small_pdf_pages=10, start_page=264, end_page=None, detext=True)
     # for i in range(1,4):
-    #     drive_ocr.split_and_ocr_on_drive(pdf_path="/run/media/vvasuki/vData/text/granthasangrahaH/history/New-History-Of-The-Marathas-Vol%d.pdf" % i,  small_pdf_pages=10, start_page=1, detext=False)
-    # drive_ocr.split_and_ocr_on_drive(pdf_path="/run/media/vvasuki/vData/text/granthasangrahaH/koshaH/upasarga-artha-candrika. vol. 1 (pra-sam).pdf", small_pdf_pages=10, detext=True, end_page=71)
-    # drive_ocr.split_and_ocr_all(dir_path="/run/media/vvasuki/vData/text/granthasangrahaH/koshaH/upasarga-artha-candrika. vol. 1 (pra-sam).pdf", small_pdf_pages=10, detext=False)
-    # drive_ocr.split_and_ocr_all( dir_path="/run/media/vvasuki/vData/text/granthasangrahaH/vedAH/sb", small_pdf_pages=10, detext=False, )
-    drive_ocr.split_and_ocr_all(dir_path="/run/media/vvasuki/vData/text/granthasangrahaH/kAvyam/shrIvaiShNavakRtam", small_pdf_pages=5, detext=False, file_pattern="[!_]*.pdf")
+    #     drive_ocr.split_and_ocr_on_drive(pdf_path="/media/vvasuki/vData/text/granthasangrahaH/history/New-History-Of-The-Marathas-Vol%d.pdf" % i,  small_pdf_pages=10, start_page=1, detext=False)
+    # drive_ocr.split_and_ocr_on_drive(pdf_path="/media/vvasuki/vData/text/granthasangrahaH/koshaH/upasarga-artha-candrika. vol. 1 (pra-sam).pdf", small_pdf_pages=10, detext=True, end_page=71)
+    # drive_ocr.split_and_ocr_all(dir_path="/media/vvasuki/vData/text/granthasangrahaH/koshaH/upasarga-artha-candrika. vol. 1 (pra-sam).pdf", small_pdf_pages=10, detext=False)
+    # drive_ocr.split_and_ocr_all( dir_path="/media/vvasuki/vData/text/granthasangrahaH/vedAH/sb", small_pdf_pages=10, detext=False, )
+    drive_ocr.split_and_ocr_all(dir_path="/media/vvasuki/vData/text/granthasangrahaH/AgamaH/vaiShNavaH/shrIvaiShNavaH", small_pdf_pages=5, detext=False, file_pattern="[!_]*.pdf")
 
 
-    # image_ops.fix_images(input_file="/run/media/vvasuki/vData/text/granthasangrahaH/gaNitam/child-texts/dav/Mathematics class 2.pdf", output_file="/run/media/vvasuki/vData/text/granthasangrahaH/gaNitam/child-texts/dav/c2.pdf", fixer=image_ops.threshold_adaptive, threshold=60)
+    # image_ops.fix_images(input_file="/media/vvasuki/vData/text/granthasangrahaH/gaNitam/child-texts/dav/Mathematics class 2.pdf", output_file="/media/vvasuki/vData/text/granthasangrahaH/gaNitam/child-texts/dav/c2.pdf", fixer=image_ops.threshold_adaptive, threshold=60)
