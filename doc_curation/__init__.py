e67164182080510ee92116eb9665a76e79a90f5b
vvasuki <vishvas.vasuki@gmail.com>
2024-12-31 11:47:50 +0530

    sd

--- doc_curation/__init__.py
+++ doc_curation/__init__.py
@@ -6,6 +6,9 @@ import json
 import logging
 
 configuration = {}
-with open('/home/vvasuki/gitland/vvasuki-git/sysconf/kunchikA/site_config.json', 'r') as handle:
-  configuration = json.load(handle)
 
+def init_configuration(file_path='/home/vvasuki/gitland/vvasuki-git/sysconf/kunchikA/site_config.json'):
+  with open(file_path, 'r') as handle:
+    configuration = json.load(handle)
+
+init_configuration()
