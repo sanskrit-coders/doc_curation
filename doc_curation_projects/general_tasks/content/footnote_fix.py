107df58ccd17fa7a98f88a7f5ea0fccb3e2cc08b
vvasuki <vishvas.vasuki@gmail.com>
2025-01-16 22:34:45 +0530

    sd

--- doc_curation_projects/general_tasks/content/footnote_fix.py
+++ doc_curation_projects/general_tasks/content/footnote_fix.py
@@ -7,7 +7,7 @@ def fix_footnotes(dir_path):
 
   # library.apply_function(fn=MdFile.transform, dir_path="/home/vvasuki/gitland/vishvAsa/AgamaH/content/AryaH/hinduism/articles/homa-variations", content_transformer=lambda x, y: footnote_helper.fix_plain_footnotes(x))
 
-  library.apply_function(fn=MdFile.transform, dir_path=dir_path, content_transformer=lambda x, y: footnote_helper.fix_plain_footnotes(x, def_pattern=footnote_helper.PLAIN_FN_DEF_GUTEN, ref_pattern=footnote_helper.PLAIN_FN_REF_GUTEN))
+  # library.apply_function(fn=MdFile.transform, dir_path=dir_path, content_transformer=lambda x, y: footnote_helper.fix_plain_footnotes(x, def_pattern=footnote_helper.PLAIN_FN_DEF_GUTEN, ref_pattern=footnote_helper.PLAIN_FN_REF_GUTEN))
 
 
   # Footnotes like (23) referring to definition (23. xyz)
@@ -20,7 +20,7 @@ def fix_footnotes(dir_path):
   # library.apply_function(fn=MdFile.transform, dir_path=dir_path, content_transformer=lambda x, y: footnote_helper.fix_plain_footnotes(x, def_pattern=r"\[(\d+)[\. ]+([^\d\)][^\]]+)\]*", def_replacement_pattern=r"\n[^\1]: \2\n", ref_pattern=r"\[(\d+)\]"))
 
   # library.apply_function(fn=MdFile.transform, dir_path=dir_path, content_transformer=lambda c, m: footnote_helper.comments_to_footnotes(c), dry_run=False)
-  # library.apply_function(fn=MdFile.transform, dir_path=dir_path, content_transformer=lambda c, m: footnote_helper.define_footnotes_near_use(c), dry_run=False)
+  library.apply_function(fn=MdFile.transform, dir_path=dir_path, content_transformer=lambda c, m: footnote_helper.define_footnotes_near_use(c), dry_run=False)
   # library.apply_function(fn=MdFile.transform, dir_path=dir_path, content_transformer=lambda c, m: footnote_helper.fix_intra_word_footnotes(c), dry_run=False)
   pass
 
@@ -37,7 +37,7 @@ def fix_footnotes_vod(dir_path):
 
 if __name__ == '__main__':
   pass
-  # fix_footnotes(dir_path="/home/vvasuki/gitland/vishvAsa/english/content/prose/hindu/indologist/max-muller/india_what_it_can_teach_us.md")
+  fix_footnotes(dir_path="/home/vvasuki/gitland/vishvAsa/notes/content/skills/research/articles/lowell_35.md")
   # fix_footnotes_ambuda(dir_path="/home/vvasuki/gitland/vishvAsa/AgamaH_vaiShNavaH/content/mAdhva-sampradAyaH/kriyA/madhva-tantra-sAra-sangrahaH/sarva-prastutiH")
   # fix_footnotes_vod(dir_path="/home/vvasuki/gitland/vishvAsa/notes/content/sapiens/branches/Aryan/satem/indo-iranian/indo-aryan/india/4_post-brit/politics/id/hindutva/articles/goel_sitArAm")
   # library.apply_function(fn=MdFile.transform, dir_path="/home/vvasuki/gitland/vishvAsa/vedAH_yajuH/content/taittirIyam/sUtram/baudhAyanaH/shrautam/sarva-prastutiH", content_transformer=lambda c, m: footnote_helper.make_ids_unique_to_be_fixed(c), dry_run=False)
