b0fa2d74ac7d5385a6a9e976860c2a78d6f08aee
vvasuki <vishvas.vasuki@gmail.com>
2026-09-06 12:29:11 +0530

    asdf

--- doc_curation/llm/gemini.py
+++ doc_curation/llm/gemini.py
@@ -1,8 +1,19 @@
 from google import genai
 
-client = genai.Client()
+from curation_utils import creds
 
-def process_file(file_path, prompt):
+client = None
+
+def get_client(api_key_path, cred_path="/home/vvasuki/gitland/vvasuki-git/sysconf/kunchikA/tokens.toml"):
+  global client
+  if client is None:
+    creds.get_toml_value(path=cred_path, key=api_key_path)
+    client = genai.Client(api_key=api_key_path)
+  return client
+
+
+def process_file(file_path, prompt, api_key_path="gemini.vv"):
+  client = get_client(api_key_path=api_key_path)
   uploaded = client.files.upload(
     file=file_path
   )
